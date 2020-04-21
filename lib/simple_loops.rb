81eecef27746a2008258706de444065e15f2d575
Rebecca Rosenberg <rebecca.ap.rosenberg@gmail.com>
2020-04-21 16:05:24 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,1 +1,16 @@
 # Write your methods here
+def loop_message_five_times(message = "Hello world.")
+  count = 0
+  while count < 5 do
+    puts "#{message}"
+    count += 1
+  end
+end
+
+def loop_message_n_times(message = "Hello moon.", number)
+  count = 0
+  while count < number do
+    puts "#{message}"
+    count += 1
+  end
+end
